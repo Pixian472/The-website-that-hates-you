065714aea91741c6299e780480bc9ac38c2f0b5e
Ryuzaki-Kazuka <59923865+Ryuzaki-Kazuka@users.noreply.github.com>
2020-02-26 14:58:58 +0000

    Batch update

---
+++ js/script.js
@@ -0,0 +1,113 @@
+
+<!DOCTYPE html>
+<html>
+  <body>
+    <p>
+      <script>
+        alert('Why are you still here, theres nothing to see here');
+        function stateChange(newState) {
+  setTimeout('', 1000000);
+
+  if(newState == -1) {
+    alert('VIDEO HAS STOPPED');
+  }
+}
+        alert('seriously, why are you here, nothings going to change, just go home');
+        function stateChange(newState) {
+  setTimeout('', 5000);
+
+  if(newState == -1) {
+    alert('VIDEO HAS STOPPED');
+  }
+}       
+        alert('I mean, i am glad you like me, but this is getting wierd.')
+        function stateChange(newState) {
+  setTimeout('', 5000);
+
+  if(newState == -1) {
+    alert('VIDEO HAS STOPPED');
+  }
+}
+       alert('Youre still here, do you ever have any human contact, do you need help?')
+       function stateChange(newState) {
+  setTimeout('', 5000);
+
+  if(newState == -1) {
+    alert('VIDEO HAS STOPPED');
+  }
+}
+      alert('Are you okay, Im worried about you, go and talk to somebody, hug your mother, say you love her.')
+      function stateChange(newState) {
+    setTimeout(function () {
+        if (newState == -1) {
+            alert('VIDEO HAS STOPPED');
+        }
+    }, 5000);
+}
+       alert('so, you got any plans for the weekend?')
+       function stateChange(newState) {
+    setTimeout(function () {
+        if (newState == -1) {
+            alert('VIDEO HAS STOPPED');
+        }
+    }, 5000);
+}
+       alert('Sounds like fun')
+       function stateChange(newState) {
+    setTimeout(function () {
+        if (newState == -1) {
+            alert('VIDEO HAS STOPPED');
+        }
+    }, 5000);
+}
+        alert('Youre still here? You sure are spending a lot time here, you could play a game, maybe go and play CSGO, its fun, you could make some friends.')
+        function stateChange(newState) {
+    setTimeout(function () {
+        if (newState == -1) {
+            alert('VIDEO HAS STOPPED');
+        }
+    }, 5000);
+}
+        alert('I swear down i will sing All star, please leave')
+        function stateChange(newState) {
+    setTimeout(function () {
+        if (newState == -1) {
+            alert('VIDEO HAS STOPPED');
+        }
+    }, 5000);
+}
+        alert('Somebody once told me the world is gonna roll me I aint the sharpest tool in the shed She was looking kind of dumb with her finger and her thumb In the shape of an "L" on her forehead Hey now, youre an all-star, get your game on, go play Hey now, youre a rock star, get the show on, get paid And all that glitters is gold Only shooting stars break the mold')
+        function stateChange(newState) {
+    setTimeout(function () {
+        if (newState == -1) {
+            alert('VIDEO HAS STOPPED');
+        }
+    }, 5000);
+}
+        alert('wait, did that backfire, you want more, well im not giving you any more')
+        function stateChange(newState) {
+    setTimeout(function () {
+        if (newState == -1) {
+            alert('VIDEO HAS STOPPED');
+        }
+    }, 5000);
+}
+        alert('you know, i have things to do, you have things to do, lets cut it straight and lets go')
+        function stateChange(newState) {
+    setTimeout(function () {
+        if (newState == -1) {
+            alert('VIDEO HAS STOPPED');
+        }
+    }, 5000);
+}
+        alert('凸(｀0´)凸')
+        function stateChange(newState) {
+    setTimeout(function () {
+        if (newState == -1) {
+            alert('VIDEO HAS STOPPED');
+        }
+    }, 5000);
+}
+       alert('ε=ε=┌( >_<)┘')
+       alert('BYE MOFUCKER')
+      
